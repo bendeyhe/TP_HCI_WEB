a1ffc0dd43ca864be7b84dc84634ae9f5cc13087
MatiasMutz <mmutz@itba.edu.ar>
2023-10-09 23:18:49 -0300

    warning fixed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -24,7 +24,7 @@ const router = useRouter()
 function login(){
     //autenticar usuario con Pinia
     storage.user = username
-    const redirectUrl = route.query.redirect || '/'
-    router.push(redirectUrl)
+    const redirectUrl = route.query.redirect || '/' // si redirect es un path "/login" puede que les funcione directo el push()
+    router.push({path: redirectUrl})
 }
 </script>
