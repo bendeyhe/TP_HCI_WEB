cc74a252943f1448d047a8d3e8ebf26bd323168b
Mati Mutz <mmutz@itba.edu.ar>
2023-10-24 18:21:56 -0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-    <v-app>
+    <v-app v-if="!isOnline()">
         <v-main>
             <AppBar/>
             <RouterView/>
+        </v-main>
+    </v-app>
+    <v-app v-else>
+        <v-main>
+            <img :src="getImageUrl('nointernet.jpg')" alt="nointernet"/>
         </v-main>
     </v-app>
 </template>
 
 <script setup>
 import {RouterView} from 'vue-router';
-import AppBar from "@/components/AppBar.vue";
+import AppBar from "@/components/AppBar.vue"
 
 const userStore = useUserStore();
 
-function isLogged() {
-    return userStore.getTokenState() !== null && userStore.getTokenState() !== '';
+function getImageUrl(name) {
+    return new URL(`../assets/${name}`, import.meta.url).href
 }
+
+function isOnline() {
+    return window.navigator.onLine;
+}
+
 </script>
 
 <script>
@@ -26,4 +36,12 @@ export default {
         await useUserStore().initialize();
     }
 }
+
 </script>
+
+<style scoped>
+img {
+    width: 72px;
+    height: 30px;
+}
+</style>
--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,40 +1,40 @@
 <template>
-  <v-app-bar scroll-behavior="elevate" class="bg-secondary">
+    <v-app-bar scroll-behavior="elevate" class="bg-secondary">
 
-    <RouterLink to="/">
-      <v-app-bar-title><h2>TooBig</h2></v-app-bar-title>
-    </RouterLink>
+        <RouterLink to="/">
+            <v-app-bar-title><h2>TooBig</h2></v-app-bar-title>
+        </RouterLink>
 
-    <SearchBar v-if="isLoggedIn"/>
-    <div class="flex-grow-1" v-else></div>
+        <SearchBar v-if="isLoggedIn"/>
+        <div class="flex-grow-1" v-else></div>
 
-    <RouterLink :to="{ name: 'create-routine' }">
-      <v-btn>Crear Rutina</v-btn>
-    </RouterLink>
+        <RouterLink :to="{ name: 'create-routine' }">
+            <v-btn>Crear Rutina</v-btn>
+        </RouterLink>
 
-    <v-menu open-on-hover v-if="isLoggedIn">
-      <template v-slot:activator="{ props }">
-        <v-btn prepend-icon="mdi-account-circle" v-bind="props">Mi Perfil</v-btn>
-      </template>
-      <v-list>
-        <v-list-item @click="goTo('/my-profile')">Perfil</v-list-item>
-        <v-list-item @click="goTo('/my-exercises')">Mis ejercicios</v-list-item>
-        <v-list-item @click="goTo('/my-routines')">Mis rutinas</v-list-item>
-        <v-list-item @click="goTo('/favourite')">Favoritos</v-list-item>
-        <v-list-item @click="logout">Cerrar sesión</v-list-item>
-      </v-list>
-    </v-menu>
-    <v-menu open-on-hover v-else>
-      <template v-slot:activator="{ props }">
-        <v-btn prepend-icon="mdi-account-circle" v-bind="props">Iniciar Sesión</v-btn>
-      </template>
-      <v-list>
-        <v-list-item @click="goTo('/login')">Iniciar sesión</v-list-item>
-        <v-list-item @click="goTo('/create-account')">Registrarse</v-list-item>
-        <v-list-item @click="goTo('/validate')">Verificar email</v-list-item>
-      </v-list>
-    </v-menu>
-  </v-app-bar>
+        <v-menu open-on-hover v-if="isLoggedIn">
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-account-circle" v-bind="props">Mi Perfil</v-btn>
+            </template>
+            <v-list>
+                <v-list-item @click="goTo('/my-profile')">Perfil</v-list-item>
+                <v-list-item @click="goTo('/my-exercises')">Mis ejercicios</v-list-item>
+                <v-list-item @click="goTo('/my-routines')">Mis rutinas</v-list-item>
+                <v-list-item @click="goTo('/favourite')">Favoritos</v-list-item>
+                <v-list-item @click="logout">Cerrar sesión</v-list-item>
+            </v-list>
+        </v-menu>
+        <v-menu open-on-hover v-else>
+            <template v-slot:activator="{ props }">
+                <v-btn prepend-icon="mdi-account-circle" v-bind="props">Iniciar Sesión</v-btn>
+            </template>
+            <v-list>
+                <v-list-item @click="goTo('/login')">Iniciar sesión</v-list-item>
+                <v-list-item @click="goTo('/create-account')">Registrarse</v-list-item>
+                <v-list-item @click="goTo('/validate')">Verificar email</v-list-item>
+            </v-list>
+        </v-menu>
+    </v-app-bar>
 </template>
 
 <script setup>
@@ -53,16 +53,16 @@ onBeforeMount(
 )
 
 const isLoggedIn = computed(() => {
-  return userStore.token;
+    return userStore.token;
 });
 
 const goTo = (route) => {
-  router.push({path: route});
+    router.push({path: route});
 };
 
 const logout = async () => {
-  await userStore.removeToken();
-  await router.push({path: '/login'});
+    await userStore.removeToken();
+    await router.push({path: '/login'});
 };
 
 
@@ -71,18 +71,18 @@ const logout = async () => {
 <style scoped>
 
 h2 {
-  font-family: Montserrat, sans-serif;
-  font-weight: bolder;
-  font-size: 200%;
-  color: #000000;
-  background-color: #8efd00;
-  padding: 20px;
-  display: inline-block;
+    font-family: Montserrat, sans-serif;
+    font-weight: bolder;
+    font-size: 200%;
+    color: #000000;
+    background-color: #8efd00;
+    padding: 20px;
+    display: inline-block;
 }
 
 .v-btn {
-  color: #8efd00;
-  margin-right: 10px;
+    color: #8efd00;
+    margin-right: 10px;
 }
 </style>
 
